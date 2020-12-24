4c3388883df64083ca0392d2006f8aa874f78791
Dilberovicka31 <dilberovicm36@gmail.com>
2020-12-23 18:19:17 -0800

    added seed file

--- schema.sql
+++ schema.sql
@@ -2,21 +2,25 @@ DROP DATABASE IF EXISTS employeesDB;
 CREATE database employeesDB;
 USE employeesDB;
 CREATE TABLE department(
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(50),
-  PRIMARY KEY (position)
+  PRIMARY KEY (id)
 );
 CREATE TABLE role(
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   title VARCHAR(30),
-  salary DECIMAL(5, 2),
-  department id INT,
-  PRIMARY KEY (position)
+  salary DECIMAL(15, 2),
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY role(department_id) REFERENCES department(id)
 );
 CREATE TABLE employee (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR (30),
   role_id INT,
   manager_id INT,
-)
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  CONSTRAINT manager_ref FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
---
+++ seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+  department(name)
+VALUES
+  ("Accounting"),
+  ("Development"),
+  ("HR");
+INSERT INTO
+  role(title, salary, department_id)
+VALUES
+  ("Engineer", 80000.00, 2),
+  ("Accountant", 60000.00, 1),
+  ("HR Representative", 50000.00, 3);
+INSERT INTO
+  employee(first_name, last_name, role_id, manager_id)
+VALUES
+  ("Mija", "Dilberovic", 1, null),
+  ("Mark", "Mullholland", 1, 1),
+  ("Molly", "Mullholland", 2, 1);
