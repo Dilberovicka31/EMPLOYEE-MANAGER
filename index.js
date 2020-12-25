54385ea3ee3a5c719b3f31aa8fdddfeea32ffcf8
Dilberovicka31 <dilberovicm36@gmail.com>
2020-12-24 18:50:07 -0800

    Created object with get functions

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const mysql = require("mysql");
-const util = require("util");
+const connection = require("./connection");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: " PASSWORD",
-  database: "employeesDB",
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected as id" + connection.threadId);
-});
-connection.query = util.promisify(connection.query);
-
-module.exports = connection;
+module.exports = {
+  getDepartment() {
+    return connection.query("SELECT * FROM department");
+  },
+  getRole() {
+    return connection.query("SELECT * FROM role");
+  },
+  getEmployee() {
+    return connection.query("SELECT * FROM employee");
+  },
+};
