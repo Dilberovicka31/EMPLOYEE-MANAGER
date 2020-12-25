24fd8627f8e44cdfab84b03a3097a539aa35cc0b
Dilberovicka31 <dilberovicm36@gmail.com>
2020-12-24 18:45:16 -0800

    created connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: " PASSWORD",
+  database: "employeesDB",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected as id" + connection.threadId);
+});
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
